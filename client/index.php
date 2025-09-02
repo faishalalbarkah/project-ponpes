a5ab1d6197e18fabc596fd74822ad0ef80401812
faishalalbarkah <faisalalbarkah1414@gmail.com>
2025-09-02 08:52:06 +0700

    Update

--- client/index.php
+++ client/index.php
@@ -2,147 +2,233 @@
 
 <!DOCTYPE html>
 <html lang="en-US">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-		
-		<meta name="description" content="Saat itu sempat saya tanya sama seorang ibu yang membawa beberapa kardus besar menuju kamar anaknya. “ Bawa apa bu?” Sang ibu, tersenyum, sambil mesem mesem “ ini stok makanan buat anak saya ustadz, hehehe”ujarnya. “ anak saya susah makan di dapur, gak enak rasanya, jadi saya bawain stok makanan supaya gak kelaparan” lanjutnya. Saya tertegun, lantas saya sampaikan, “.." />
-    <link rel="stylesheet" href="../assets/css/ponpes.css">
-    <link rel="stylesheet" href="../assets/css/madding.css">
-<link rel="stylesheet" href="../assets/css/splide.css">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+	<meta name="description" content="Saat itu sempat saya tanya sama seorang ibu yang membawa beberapa kardus besar menuju kamar anaknya. “ Bawa apa bu?” Sang ibu, tersenyum, sambil mesem mesem “ ini stok makanan buat anak saya ustadz, hehehe”ujarnya. “ anak saya susah makan di dapur, gak enak rasanya, jadi saya bawain stok makanan supaya gak kelaparan” lanjutnya. Saya tertegun, lantas saya sampaikan, “.." />
+	<link rel="stylesheet" href="../assets/css/ponpes.css">
+	<link rel="stylesheet" href="../assets/css/madding.css">
+	<link rel="stylesheet" href="../assets/css/splide.css">
+	<style>
+		.nav-link {
+			text-decoration: none;
+			color: #059605;
+		}
+
+		.nav-link:hover {
+			text-decoration: underline;
+		}
+
+		.card {
+			background: white;
+			border-radius: 12px;
+			box-shadow: 0 4px 20px rgba(0, 0, 0, .06);
+			padding: 18px;
+			margin-bottom: 18px;
+		}
+
+		table {
+			width: 100%;
+			border-collapse: collapse;
+			background: white;
+		}
+
+		th,
+		td {
+			border: 1px solid #e5e7eb;
+			padding: 10px 12px;
+			text-align: left;
+			vertical-align: top;
+		}
+
+		th {
+			background: #f3f4f6;
+			font-weight: 700;
+		}
+	</style>
 
 	<title>Makanan Dapur di pondok pesantren - Pondok Pesantren Assyifa Sagalaherang Subang</title>
-	
-  </head>
-	
-	<body>
-	    <div class="mad__wrapper layout_mading">
-		    <section class="mad__header madclear">
-				<div class="mad__nav">
-				    <div class="ext__container">
-				    	<div class="mad__flex">
-					    	<div class="mad__logo">
-						    	<a href="" class="custom-logo-link" rel="home"><img width="600" height="165" src="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2025/06/cropped-pondok-scaled-1.png" class="custom-logo" alt="Pondok Pesantren Assyifa Sagalaherang Subang" decoding="async" srcset="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2025/06/cropped-pondok-scaled-1.png 600w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2025/06/cropped-pondok-scaled-1-300x83.png 300w" sizes="(max-width: 600px) 100vw, 600px" /></a>							</div>
-							<div id="site-header-menu" class="site-header-menu">
-							<div class="newnav nav-desktop nav-mobile"><ul id="newid" class="newul accordion"><li id="menu-item-1447" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-1447"><a href="">Home</a></li>
-		<li id="menu-item-138" class="profil"><a href="">Profil</a>
-				  </div>
-				</div>
-			  </div>
-			</div>
-		 </div>
-		</section>
-		    			
-			    <!-- yang berjalan ke samping -->
-				<div class="mad_ticker">
-				    <div class="ext__container">
-				        <div class="sekilas">
-				    	    <span class="re_info">School Info</span>
-				    	</div>
-				        <div class="mad_today">						
-							<span>Tuesday, 26 Aug 2025</span>
-				    	</div>
-				    	<div class="runscroll">
-					    	<div class="scroll text4">
-						    	<ul class="scrollin my-news-ticker">
-							    	<li>Ahlan Wa Sahlan di Web Ponpes As-Syifa Sagalaherang||  Info PMB Ponpes Assyifa Sagalaherang<a href="http://bit.ly/PMB-Assyifa"> Klik  Disini</a>
-									</li>								
-                                 </ul>
-								<ul class="scrollin my-news-ticker">
-							    	<li>Ahlan Wa Sahlan di Web Ponpes As-Syifa Sagalaherang||  Info PMB Ponpes Assyifa Sagalaherang<a href="http://bit.ly/PMB-Assyifa"> Klik  Disini</a>
-									</li>								
-                              </ul>
+
+</head>
+
+<body>
+	<div class="mad__wrapper layout_mading">
+		<section class="mad__header madclear">
+			<div class="mad__nav">
+				<div class="ext__container">
+					<div class="mad__flex">
+						<div class="mad__logo">
+							<a href="" class="custom-logo-link" rel="home"><img width="600" height="165" src="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2025/06/cropped-pondok-scaled-1.png" class="custom-logo" alt="Pondok Pesantren Assyifa Sagalaherang Subang" decoding="async" srcset="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2025/06/cropped-pondok-scaled-1.png 600w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2025/06/cropped-pondok-scaled-1-300x83.png 300w" sizes="(max-width: 600px) 100vw, 600px" /></a>
+						</div>
+						<div id="site-header-menu" class="site-header-menu">
+							<div class="newnav nav-desktop nav-mobile">
+								<ul id="newid" class="newul accordion">
+									<li id="menu-item-1447" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-1447"><a href="">Home</a></li>
+									<li id="menu-item-138" class="profil"><a href="">Profil</a>
 							</div>
 						</div>
 					</div>
 				</div>
-	 
+			</div>
+		</section>
 
-		<div class="mading_bread">
-	    	<div class="bread_inner ext__container">
-	            <div class="breadcrumbs" itemscope itemtype="https://schema.org/WebPage"><div class="inbread" itemprop="breadcrumb">You are here :<span class="here"></span> <span><a href="https://assyifasagalaherang.ponpes.id/" itemprop="url">Home</a></span> - <span>Makanan Dapur di pondok pesantren</span></div>
-			    </div>       	
-		</div>
-    </div>
-		<section class="mad_single">
-	        <div class="ext__container">
-			    
-			    <div class="mad_konten madclear">
-						    
-							<div class="primary_little">
-							    <div class="publish_date madclear">
-								    <span class="mdate">21</span>
-									<span class="mmonth">July</span>
-									<span class="mmonth">2023</span>
-								</div>
-
-	   <!-- Icon dan share Sosmednya -->
-	<div class="share">
-		<i class="icon-mad-share"></i>
-		<a href="" target="_blank" title="Share to Facebook">
-		    <i class="icon-mad-facebook"></i>
-		</a>
-		<a href="" target="_blank" title="Share to Twitter">
-			<i class="icofont-x"></i>
-		</a>
-		<a target="_blank" href="" title="Share to WhatsApp">
-			<i class="icon-mad-whatsapp"></i>
-		</a>
-		<a href="" target="_blank" title="Share to Telegram">
-			<i class="icon-mad-telegram"></i>
-		</a>
-	</div>	
-
-</div>
-							
-							<div class="primary_big">
-						    	<h1 class="mading_heading">Makanan Dapur di pondok pesantren</h1>
-				                <div class="after_title">
-								    Fri, 21 July 2023 <i class="icon-mad-heart-half"></i> Read 2419x						    	</div>
-							    
-							<div class="mading_featured">
-								<img width="960" height="1280" src="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15.jpg" class="attachment-full size-full wp-post-image" alt="" decoding="async" srcset="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15.jpg 960w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-225x300.jpg 225w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-768x1024.jpg 768w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-300x400.jpg 300w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-110x147.jpg 110w" sizes="(max-width: 960px) 100vw, 960px" />							    
-						    </div>
-							
-						<div class="card">
-						<div style="display:flex;justify-content:space-between;align-items:center;">
-							<h2 style="margin:0;">Menu Makanan Mingguan</h2>
-							<a class="btn" href="menu_tambah.php">Tambah</a>
-						</div>
-						<table>
-							<tr>
-										<th>Hari</th>
-								<th>Makan Pagi</th>
-								<th>Makan Siang</th>
-								<th>Makan Malam</th>
-								<th>Ekstra</th>
-							<th>Aksi</th>
-							</tr>
-							
-						</table>
-						</div>
-
-		        </section>	   
-	              </div>
-                    </div>
-                      </div>
-                        </div>					
-                         </div>
-						</div>
+		<!-- yang berjalan ke samping -->
+		<div class="mad_ticker">
+			<div class="ext__container">
+				<div class="sekilas">
+					<span class="re_info">School Info</span>
+				</div>
+				<div class="mad_today">
+					<span id="date1"></span>
+				</div>
+				<div class="runscroll">
+					<div class="scroll text4">
+						<ul class="scrollin my-news-ticker">
+							<li>Ahlan Wa Sahlan di Web Ponpes As-Syifa Sagalaherang|| Info PMB Ponpes Assyifa Sagalaherang<a href="http://bit.ly/PMB-Assyifa" class="nav-link"> Klik Disini</a>
+							</li>
+						</ul>
+						<ul class="scrollin my-news-ticker">
+							<li>Ahlan Wa Sahlan di Web Ponpes As-Syifa Sagalaherang|| Info PMB Ponpes Assyifa Sagalaherang<a href="http://bit.ly/PMB-Assyifa" class="nav-link"> Klik Disini</a>
+							</li>
+						</ul>
 					</div>
 				</div>
-		    </div>
-			
-			
-			<div class="footer">
-			    <div class="copyright">
-					Ponpes As-Syifa Sagalaherang			    	
-                </div>
 			</div>
+		</div>
+
+
+		<div class="mading_bread">
+			<div class="bread_inner ext__container">
+				<div class="breadcrumbs" itemscope itemtype="https://schema.org/WebPage">
+					<div class="inbread" itemprop="breadcrumb">You are here :<span class="here"></span> <span><a href="http://127.0.0.1/project-ponpes/client/index.php"
+								itemprop="url" class="nav-link">Home</a>
+						</span> - <span>Makanan Dapur di pondok pesantren</span></div>
+				</div>
+			</div>
+		</div>
+		<section class="mad_single">
+			<div class="ext__container">
+
+				<div class="mad_konten madclear">
+
+					<div class="primary_little">
+						<div class="publish_date madclear">
+							<span class="mdate"></span>
+							<span class="mmonth"></span>
+							<span class="myear"></span>
+						</div>
+
+						<!-- Icon dan share Sosmednya -->
+						<div class="share">
+							<i class="icon-mad-share"></i>
+							<a href="" target="_blank" title="Share to Facebook">
+								<i class="icon-mad-facebook"></i>
+							</a>
+							<a href="" target="_blank" title="Share to Twitter">
+								<i class="icofont-x"></i>
+							</a>
+							<a target="_blank" href="" title="Share to WhatsApp">
+								<i class="icon-mad-whatsapp"></i>
+							</a>
+							<a href="" target="_blank" title="Share to Telegram">
+								<i class="icon-mad-telegram"></i>
+							</a>
+						</div>
+
+					</div>
+
+					<div class="primary_big">
+						<h1 class="mading_heading">Makanan Dapur di pondok pesantren</h1>
+						<div class="after_title" id="date2">
+							<i class="icon-mad-heart-half"></i>
+						</div>
+
+						<div class="mading_featured">
+							<img width="960" height="1280" src="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15.jpg" class="attachment-full size-full wp-post-image" alt="" decoding="async" srcset="https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15.jpg 960w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-225x300.jpg 225w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-768x1024.jpg 768w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-300x400.jpg 300w, https://assyifasagalaherang.ponpes.id/wp-content/uploads/2023/09/photo_2023-09-04_06-12-15-110x147.jpg 110w" sizes="(max-width: 960px) 100vw, 960px" />
+						</div>
+
+						<div class="card">
+							<div style="display:flex;justify-content:space-between;align-items:center;">
+								<h2 style="margin:0;">Menu Makanan Mingguan</h2>
+							</div>
+							<table>
+								<tr>
+									<th>Hari</th>
+									<th>Makan Pagi</th>
+									<th>Makan Siang</th>
+									<th>Makan Malam</th>
+									<th>Ekstra</th>
+								</tr>
+								<?php
+								$res = $conn->query("SELECT * FROM menu_makanan ORDER BY FIELD(hari,'Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu')");
+								while ($row = $res->fetch_assoc()) {
+									echo '<tr>';
+									echo '<td>' . nl2br(htmlspecialchars($row['hari'])) . '</td>';
+									echo '<td>' . nl2br(htmlspecialchars($row['makan_pagi'])) . '</td>';
+									echo '<td>' . nl2br(htmlspecialchars($row['makan_siang'])) . '</td>';
+									echo '<td>' . nl2br(htmlspecialchars($row['makan_malam'])) . '</td>';
+									echo '<td>' . nl2br(htmlspecialchars($row['ekstra'])) . '</td>';
+									echo '</tr>';
+								}
+								?>
+							</table>
+						</div>
+
+		</section>
+		<!-- </div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div> -->
+
+
+		<div class="footer">
+			<div class="copyright">
+				Ponpes As-Syifa Sagalaherang
+			</div>
+		</div>
 
 
 
-      <script src="./src/asset/js/ponpes.js"></script>
-	</body>
+		<script src="./src/asset/js/ponpes.js"></script>
+		<script>
+			const now = new Date();
+
+			// Format: Tuesday, 26 Aug 2025
+			const options1 = {
+				weekday: 'long',
+				day: '2-digit',
+				month: 'short',
+				year: 'numeric'
+			};
+			document.getElementById("date1").textContent = now.toLocaleDateString('en-US', options1);
+
+			// Format: Fri, 21 July 2023
+			const options2 = {
+				weekday: 'short',
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			};
+			document.getElementById("date2").textContent = now.toLocaleDateString('en-US', options2);
+
+			const day = now.getDate(); // tanggal
+			const monthLong = now.toLocaleString('en-US', {
+				month: 'long'
+			}); // July
+			const year = now.getFullYear(); // 2025
+
+			// masukkan ke HTML
+			document.querySelector('.mdate').textContent = day;
+			document.querySelector('.mmonth').textContent = monthLong;
+			document.querySelector('.myear').textContent = year;
+		</script>
+</body>
+
 </html>
-
